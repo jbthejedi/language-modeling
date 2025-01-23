aeca12de5dea58332b31a521292801c8596725e3
Justin Barry <justinbarry@Justins-MacBook-Air.local>
2025-01-23 08:13:55 +0700

    adding batch norm impl

---
+++ layernorm.py
@@ -0,0 +1,18 @@
+import torch
+
+class BatchNorm1d:
+    def __init__(self, dim, eps=1e-5, momentum=0.1):
+        self.eps = eps
+        self.gamma = torch.ones(dim)
+        self.beta = torch.zeros(dim)
+
+    def __call__(self, x):
+        xmean = x.mean(1, keepdim=True)
+        xvar = x.var(1, keepdim=True)
+        xhat = (x - xmean) / torch.sqrt(xvar + self.eps)
+        self.out = self.gamma * xhat + self.beta
+        return self.out
+
+    def parameters(self):
+        return [self.gamma, self.beta]
+
