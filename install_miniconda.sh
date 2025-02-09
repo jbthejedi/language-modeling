41aaf9883962db381051a36e0856217204e7a447
Justin Barry <justinbarry@Justins-MacBook-Air.local>
2025-02-09 10:44:56 +0700

    remove shortcut to home

--- install_miniconda.sh
+++ install_miniconda.sh
@@ -1,4 +1,4 @@
-mkdir -p ~/workspace/miniconda3
-wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/workspace/miniconda3/miniconda.sh
-bash ~/workspace/miniconda3/miniconda.sh -b -u -p ~/workspace/miniconda3
-rm ~/workspace/miniconda3/miniconda.sh
+mkdir -p /workspace/miniconda3
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O /workspace/miniconda3/miniconda.sh
+bash /workspace/miniconda3/miniconda.sh -b -u -p /workspace/miniconda3
+rm /workspace/miniconda3/miniconda.sh
